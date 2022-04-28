690b82ded87c9acee59215408c03f88a233cca2e
Bas950 <me@bas950.com>
2022-04-28 22:31:48 +0200

    feat(RCD-12): allow revert commits

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 module.exports = {
 	plugins: ["commitlint-plugin-function-rules"],
 	parserPreset: {
-		parserOpts: { headerPattern: /^(\w*)(?:\(([A-Z]+-[0-9]+)\))?!?: (.*)$/ }
+		parserOpts: { headerPattern: /^(\w*)(?:\((.*)\))?!?: (.*)$/ }
 	},
 	rules: {
-		"scope-case": [2, "always", "upper-case"],
-		"body-leading-blank": [1, "always"],
-		"footer-leading-blank": [1, "always"],
+		"body-leading-blank": [2, "always"],
+		"footer-leading-blank": [2, "always"],
 		"header-max-length": [2, "always", 72],
 		"subject-case": [
 			2,
@@ -35,14 +34,46 @@ module.exports = {
 			2,
 			"always",
 			parsed => {
-				if (parsed.scope?.match(/([A-Z]+-[0-9]+)/)) {
-					return [true];
+				if (parsed.scope && parsed.scope.toUpperCase() !== parsed.scope) {
+					if (!parsed.raw?.startsWith("revert")) {
+						return [false, "scope must be upper-case"];
+					} else {
+						if (
+							parsed.raw?.match(
+								/^(?:revert\(([A-Z]+-[0-9]+)\)!?: )(\w*)(?:\(([A-Z]+-[0-9]+)\))!?: (.*)$/
+							)
+						) {
+							return [true];
+						} else {
+							return [false, "scope must be upper-case"];
+						}
+					}
+				}
+				if (parsed.scope?.match(/[A-Z]+-[0-9]+/)) {
+					if (parsed.type === "revert") {
+						return [
+							false,
+							"revert commits should have the full commit name that is being reverted"
+						];
+					} else {
+						return [true];
+					}
 				}
 				if (parsed.raw?.match(/^(\w*)(?:\(([A-Z]+-[0-9]+)\))!?: (.*)$/)) {
 					return [true];
 				}
 				if (parsed.raw?.match(/^(\w*)(?:\(([A-Z]+-[0-9]+)\))?!?: (.*)$/)) {
-					return [false, `a scope must be set`];
+					if (parsed.type === "revert") {
+						return [
+							false,
+							"revert commits should have the associated JIRA ID includes in the revert scope"
+						];
+					} else {
+						return [false, `a scope must be set`];
+					}
+				}
+				if (!parsed.subject) {
+					return [false, `a subject must be set`];
 				}
 				return [false, `scope must be a JIRA issue ID`];
 			}
--- index.test.js
+++ index.test.js
@@ -2,115 +2,199 @@ const lint = require("@commitlint/lint");
 const functionRules = require("commitlint-plugin-function-rules");
 const { rules, parserOpts } = require(".");
 
-const lintMessage = async message => {
-	const m = message.replace(/^\s+/, "").trim();
-	const result = await lint.default(m, rules, {
+const lintMessage = async message =>
+	lint.default(message.replace(/^\s+/, "").trim(), rules, {
 		parserOpts,
 		plugins: {
 			"commitlint-plugin-function-rules": functionRules
 		}
 	});
 
-	return result;
-};
-
 test("a valid commit message", async () => {
-	const result = await lintMessage("feat(RCD-1): a valid commit message");
+	const { valid, errors, warnings } = await lintMessage(
+		"feat(RCD-1): a valid commit message"
+	);
 
-	expect(result.valid).toBe(true);
-	expect(result.errors).toStrictEqual([]);
-	expect(result.warnings).toStrictEqual([]);
+	expect(valid).toBe(true);
+	expect(errors).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
 });
 
 test("a valid multi line commit", async () => {
-	const result = await lintMessage(
+	const { valid, errors, warnings } = await lintMessage(
 		`test(RCD-1): a valid angular commit with a scope
 
      Some content in the body`
 	);
 
-	expect(result.valid).toBe(true);
-	expect(result.errors).toStrictEqual([]);
-	expect(result.warnings).toStrictEqual([]);
+	expect(valid).toBe(true);
+	expect(errors).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
 });
 
 test("a leading blank line after header", async () => {
-	const result = await lintMessage(
+	const { valid, errors, warnings } = await lintMessage(
 		`test(RCD-1): a valid angular commit with a scope
      Some content in the body`
 	);
 
-	expect(result.valid).toBe(true);
-	expect(result.errors).toStrictEqual([]);
-	expect(result.warnings[0].message).toBe("body must have leading blank line");
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("body must have leading blank line");
+	expect(warnings).toStrictEqual([]);
 });
 
 test("an invalid scope", async () => {
-	const result = await lintMessage(`no: no is not not an invalid commit type`);
+	const { valid, errors, warnings } = await lintMessage(
+		`no: no is not not an invalid commit type`
+	);
 
-	expect(result.valid).toBe(false);
-	expect(result.errors[0].message).toBe(
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe(
 		"type must be one of [build, ci, docs, feat, fix, perf, refactor, revert, style, test]"
 	);
-	expect(result.warnings).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
 });
 
 test("no type", async () => {
-	const result = await lintMessage(
+	const { valid, errors, warnings } = await lintMessage(
 		`feat: no is not not an invalid commit type`
 	);
 
-	expect(result.valid).toBe(false);
-	expect(result.errors[0].message).toBe("a scope must be set");
-	expect(result.warnings).toStrictEqual([]);
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("a scope must be set");
+	expect(warnings).toStrictEqual([]);
 });
 
 test("an invalid type", async () => {
-	const result = await lintMessage(
+	const { valid, errors, warnings } = await lintMessage(
 		`feat(heya): no is not not an invalid commit type`
 	);
 
-	expect(result.valid).toBe(false);
-	expect(result.errors[0].message).toBe("scope must be upper-case");
-	expect(result.warnings).toStrictEqual([]);
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("scope must be upper-case");
+	expect(warnings).toStrictEqual([]);
 });
 
 test("an invalid jira id", async () => {
-	const result = await lintMessage(
+	const { valid, errors, warnings } = await lintMessage(
 		`feat(HEYA): no is not not an invalid commit type`
 	);
 
-	expect(result.valid).toBe(false);
-	expect(result.errors[0].message).toBe("scope must be a JIRA issue ID");
-	expect(result.warnings).toStrictEqual([]);
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("scope must be a JIRA issue ID");
+	expect(warnings).toStrictEqual([]);
 });
 
 test("a long header", async () => {
-	const result = await lintMessage(
+	const { valid, errors, warnings } = await lintMessage(
 		`test(RCD-1): that its an error when there is ia realllllllllllllllllllllly long header`
 	);
 
-	expect(result.valid).toBe(false);
-	expect(result.errors[0].message).toBe(
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe(
 		"header must not be longer than 72 characters, current length is 86"
 	);
-	expect(result.warnings).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
 });
 
 test("message header with ! in it", async () => {
-	const result = await lintMessage(`test!: with a breaking change in the type`);
+	const { valid, errors, warnings } = await lintMessage(
+		`test!: with a breaking change in the type`
+	);
 
-	expect(result.valid).toBe(false);
-	expect(result.errors[0].message).toBe("a scope must be set");
-	expect(result.warnings).toStrictEqual([]);
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("a scope must be set");
+	expect(warnings).toStrictEqual([]);
 });
 
 test("message header with ! in it and a scope", async () => {
-	const result = await lintMessage(
+	const { valid, errors, warnings } = await lintMessage(
 		`test(RCD-1)!: with a breaking change in the type`
 	);
 
-	expect(result.valid).toBe(true);
-	expect(result.errors).toStrictEqual([]);
-	expect(result.warnings).toStrictEqual([]);
+	expect(valid).toBe(true);
+	expect(errors).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
+});
+
+test("a subject must be set", async () => {
+	const { valid, errors, warnings } = await lintMessage(`test(RCD-1):`);
+
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("a subject must be set");
+	expect(warnings).toStrictEqual([]);
+});
+
+test("a colon must be set", async () => {
+	const { valid, errors, warnings } = await lintMessage(
+		`test(RCD-1) dwadwadwa`
+	);
+
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("a subject must be set");
+	expect(warnings).toStrictEqual([]);
+});
+
+test("JIRA ID key must be upper case", async () => {
+	const { valid, errors, warnings } = await lintMessage(
+		`test(rcd-1): a valid commit message`
+	);
+
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe("scope must be upper-case");
+	expect(warnings).toStrictEqual([]);
+});
+
+test("you should be able to revert commits", async () => {
+	const { valid, errors, warnings } = await lintMessage(
+		`revert(RCD-2): feat(RCD-1): a valid commit message`
+	);
+
+	expect(valid).toBe(true);
+	expect(errors).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
+});
+
+test("revert commits should have the full commit name that is being reverted", async () => {
+	const { valid, errors, warnings } = await lintMessage(
+		`revert(RCD-1): a valid commit message`
+	);
+
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe(
+		"revert commits should have the full commit name that is being reverted"
+	);
+	expect(warnings).toStrictEqual([]);
+});
+
+test("revert commits should have the associated JIRA ID", async () => {
+	const { valid, errors, warnings } = await lintMessage(
+		`revert: test(RCD-1): a valid commit message`
+	);
+
+	expect(valid).toBe(false);
+	expect(errors[0].message).toBe(
+		"revert commits should have the associated JIRA ID includes in the revert scope"
+	);
+	expect(warnings).toStrictEqual([]);
+});
+
+test("you should be able to revert commits and make breaking changes in them", async () => {
+	const { valid, errors, warnings } = await lintMessage(
+		`revert(RCD-2)!: feat(RCD-1): a valid commit message`
+	);
+
+	expect(valid).toBe(true);
+	expect(errors).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
+});
+
+test("you should be able to revert commits that have breaking changes and make breaking changes in them", async () => {
+	const { valid, errors, warnings } = await lintMessage(
+		`revert(RCD-2)!: feat(RCD-1)!: a valid commit message`
+	);
+
+	expect(valid).toBe(true);
+	expect(errors).toStrictEqual([]);
+	expect(warnings).toStrictEqual([]);
 });
